8162ba61ba9aa91d39f3c4bd3994eb5ca5f91b7e
Shanisahotra <jonathanzeeshan25@gmail.com>
2024-03-20 14:21:13 +0500

    ok

--- blood-mamagement/src/Components/Navbar.jsx
+++ blood-mamagement/src/Components/Navbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { BiDonateBlood } from "react-icons/bi";
 
 const Navbar = ({ isLoggedIn }) => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
+        <div>
+        <BiDonateBlood />
         <Link to="/" className="admin-icon">Blood Management System</Link>
+        </div>
         {isLoggedIn ? (
           <Link to="/logout" className="admin-icon">Logout</Link>
         ) : (
--- blood-management-backend/index.js
+++ blood-management-backend/index.js
@@ -1,8 +1,32 @@
-const express = require('express');
+ // 'mongodb://127.0.0.1:27017/Products'
+
+const express = require("express");
+const mongoose = require('mongoose');
 const app = express();
+const connectDB = async ()=>{
+    mongoose.connect('mongodb://localhost:27017/Student');
+    const productSchema = new mongoose.Schema({});
+    const product = mongoose.model('students',productSchema);
+    const data = await product.find();
+    console.log(data);
+     console.log('App is working');
 
-app.get("/",(req,resp)=>{
-    resp.send("app is working");
-});
+} 
+connectDB();
 
-app.listen(5000)
+app.listen(4000);
+
+// const {MongoClient} = require('mongodb');
+// const url = 'mongodb://localhost:27017';
+// const database = 'Products'
+// const client = new MongoClient(url);
+
+// async function getData(){
+//     let result = await client.connect();
+//     let db = result.db(database);
+//     let collection = db.collection('product');
+//     let response = await collection.find({}).toArray();
+//     console.log(response);
+// }
+
+// getData();
